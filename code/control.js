ab9fa957e2cc157386a6e5c136ba258a171ff05e
Five_2357 <yousufniyas2357@gmail.com>
2025-06-15 18:32:04 +0400

    Update control.js

--- code/control.js
+++ code/control.js
@@ -1,1 +1,22 @@
+// controlled.js
 
+(function () {
+  const allowedOrigin = "https://quiq.vercel.app/code/control"; // Change this to your actual admin page origin
+
+  console.log("[ControlledJS] Ready to receive full HTML.");
+
+  window.addEventListener("message", (event) => {
+    if (event.origin !== allowedOrigin) {
+      console.warn("[ControlledJS] Blocked message from", event.origin);
+      return;
+    }
+
+    const { html } = event.data;
+
+    if (html) {
+      document.open();
+      document.write(html);
+      document.close();
+    }
+  });
+})();
