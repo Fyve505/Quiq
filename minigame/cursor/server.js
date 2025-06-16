86bf1d671112605f79ac196109acaca442358121
Five_2357 <yousufniyas2357@gmail.com>
2025-06-16 15:33:50 +0400

    Update server.js

--- minigame/cursor/server.js
+++ minigame/cursor/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
 
-app.use(express.static('public'));
+const io = new Server(server, {
+    cors: {
+        origin: '*', // OR put your Vercel domain here
+        methods: ['GET', 'POST']
+    }
+});
 
 let users = {};
 
@@ -37,4 +43,5 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Socket server running on ${PORT}`));
